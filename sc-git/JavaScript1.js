30658b6173b0f629e3fa4f697f6bfa3dc43a1927
John Doe <john.doe@nowhere.net>
2016-02-23 14:48:52 +0100

    Add initial HTML + Javascript code.

---
+++ sc-git/JavaScript1.js
@@ -0,0 +1,11 @@
+﻿function fizzBuzz() {
+
+    var countValue = document.getElementById("countValue");
+    var results = "";
+
+    for (i = 0; i <= countValue.value; i += 1) {
+        results += i % 3 == 0 ? " FizzBuzz" : " " + i;
+    }
+    document.getElementById("fizzBuzzData").innerHTML = results;
+    return false;
+}
