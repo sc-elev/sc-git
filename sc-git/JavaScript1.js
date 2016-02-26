9dc6f983690b0d2fc5656faeac4a34dd5759be4d
John Doe <john.doe@nowhere.net>
2016-02-26 08:59:44 +0100

    Javascript1: Reset correct i loop variable

--- sc-git/JavaScript1.js
+++ sc-git/JavaScript1.js
@@ -3,8 +3,8 @@
     var countValue = document.getElementById("countValue");
     var results = "";
 
-    for (j = 0; j <= countValue.value; j += 1) {
-        results += j % 3 == 0 ? " FizzBuzz" : " " + j;
+    for (i= 0; .j <= countValue.value; .j += 1) {
+        results += i % 3 == 0 ? " FizzBuzz" : " " + i;
     }
     document.getElementById("fizzBuzzData").innerHTML = results;
     return false;
