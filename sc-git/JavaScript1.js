c32926dfa547b0aef6b8ec41e5ac84187a937a2f
NiklasSonestedt <apelzin@me.com>
2016-02-26 10:33:23 +0100

    Javascript1.js

--- sc-git/JavaScript1.js
+++ sc-git/JavaScript1.js
@@ -3,8 +3,20 @@
     var countValue = document.getElementById("countValue");
     var results = "";
 
-    for (i = 0; i <= countValue.value; i += 1) {
-        results += i % 3 == 0 ? " FizzBuzz" : " " + i;
+    for (i = 0; i <= countValue.value; i += 1) 
+    {
+    	results = results + i +": ";
+    	if (i%3==0)
+    	{
+    		results = results + "Fizz";
+    	}
+    	if (i%4==0)
+    	{
+    		results = results + "Buzz";
+    	}
+    	results = results + "</br>";
+//        results += i % 3 == 0 ? " FizzBuzz" : " " + i;
+//skräp
     }
     document.getElementById("fizzBuzzData").innerHTML = results;
     return false;
