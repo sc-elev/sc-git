e7dc6b4026d8ddce2d9d108a0b49a9c2d5cec413
Martin <martin@example.com>
2016-02-26 12:35:56 +0100

    JavaScript.js

--- sc-git/JavaScript1.js
+++ sc-git/JavaScript1.js
@@ -1,11 +1,13 @@
 ﻿function fizzBuzz() {
-
+    //bugfix
     var countValue = document.getElementById("countValue");
     var results = "";
-
+    //bugfix
     for (i = 0; i <= countValue.value; i += 1) {
         results += i % 3 == 0 ? " FizzBuzz" : " " + i;
     }
+    //bugfix
     document.getElementById("fizzBuzzData").innerHTML = results;
     return false;
+    //bugfix
 }
