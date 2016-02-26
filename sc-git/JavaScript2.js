d9c3550e4b14137c8ba40969d36dabd296784d51
NiklasSonestedt <apelzin@me.com>
2016-02-26 10:42:23 +0100

    adding new file Javascript2.js

---
+++ sc-git/JavaScript2.js
@@ -0,0 +1,23 @@
+﻿function fizzBuzz() {
+
+    var countValue = document.getElementById("countValue");
+    var results = "";
+
+    for (i = 0; i <= countValue.value; i += 1) 
+    {
+    	results = results + i +": ";
+    	if (i%3==0)
+    	{
+    		results = results + "Fizz";
+    	}
+    	if (i%4==0)
+    	{
+    		results = results + "Buzz";
+    	}
+    	results = results + "</br>";
+//        results += i % 3 == 0 ? " FizzBuzz" : " " + i;
+//skräp
+    }
+    document.getElementById("fizzBuzzData").innerHTML = results;
+    return false;
+}
